bd3b934e6cef976bbb7f0dc25d3c380faf942ccd
Carlo Enrico <carlo.enrico@uol.com.br>
2018-07-22 00:02:49 -0300

    .

--- getInfo.js
+++ getInfo.js
@@ -1,62 +1,13 @@
-import data from './data.json'
+import data from './data/data.json'
 import axios from 'axios'
 
 const players = [
   {
-    name: 'vimeo'
+    name: 'vimeo',
+    limiter: '/'
   },
   {
-    name: 'youtube'
+    name: 'youtube',
+    limiter: 'v='
   }
 ]
-
-const getLastIndex = (el, search) => el.slice(el.lastIndexOf(search) + search.length)
-
-const getPlayer = url => {
-  const [{name = ''} = {}] = players.filter(({name}) => url.indexOf(name) !== -1)
-  return name
-}
-
-const getVideoID = (url, player) => {
-  const players = {
-    vimeo: () => getLastIndex(url, '/'),
-    youtube: () => getLastIndex(url, 'v=')
-  }
-  return (players[player])()
-}
-
-const getData = (url = '') => axios(url)
-
-const vimeo = vID => {
-  const endpoint = `https://vimeo.com/api/oembed.json?url=https://vimeo.com/${vID}`
-  // const data = getData(endpoint)
-}
-
-const youtube = vID => {
-  const endpoint = `https://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=${vID}&format=json`
-  // return getData(endpoint)
-}
-
-const getVideoInfo = (vID, vType) => {
-  const types = {
-    vimeo: vimeo(vID),
-    youtube: youtube(vID),
-    none: Promise.resolve({})
-  }
-  return (types[vType] || types['none'])()
-}
-
-const {log} = console
-
-log(
-  getPlayer('https://www.youtube.com/watch?v=KtDwdoxQL4A')
-)
-log(
-  getPlayer('https://vimeo.com/102553634')
-)
-log(
-  getVideoID('https://www.youtube.com/watch?v=KtDwdoxQL4A', 'youtube')
-)
-log(
-  getVideoID('https://vimeo.com/131191237', 'vimeo')
-)
